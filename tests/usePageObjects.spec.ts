d0fbce3526dff40883a0271907cb5155e96208e8
Lansing1266 <benediktas.gilis@gmail.com>
2025-04-30 15:13:35 +0300

    testing

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -35,7 +35,7 @@ test('parametrized methods @smoke', async({page}) => {
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
 })
 
-test.only('testing with argos ci', async({page}) => {
+test.only('testing with argos ci1', async({page}) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await argosScreenshot(page, "form layouts page");
